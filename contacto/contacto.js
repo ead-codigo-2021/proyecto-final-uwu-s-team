c93e6272c68dd52b28b5b1ba25d1f8c7d4f9534a
catalinariverost <catalinariverostroncoso@gmail.com>
2021-11-12 17:59:08 -0300

    actualización
    uso de js más nuevos botones junto a un aviso

---
+++ contacto/contacto.js
@@ -0,0 +1,6 @@
+// alerta
+const alertButton = document.querySelector(".button--alert");
+alertButton.addEventListener("click", function (event) {
+	event.preventDefault();
+	alert("Alerta ONEMIS");
+});
--- productos/js/productors.js
+++ productos/js/productors.js
@@ -1,11 +1,70 @@
-console.log("ola");
-const readMore = (id) => {
-    var element = document.getElementById(id);
-    element.classList.toggle("truncate");
-    console.log(element);
-  }
-  
-  function haceAlgo() {
-    console.log("entro a la funcion")
-    
-  }
+// alerta
+const alertButton = document.querySelector(".button--alert");
+alertButton.addEventListener("click", function (event) {
+	event.preventDefault();
+	alert("¿Seguro que desea continuar?");
+});
+
+document
+	.querySelector(".button--confirm")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--ok")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--ready")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--ya")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--listo")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--listo2")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--listo3")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
+
+  document
+	.querySelector(".button--listo4")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		const userConfirms = confirm("¿Seguro que desea continuar?");
+		console.log(userConfirms);
+	});
