dd475c509c79053dc58e5d4269ac076e522d7845
catalinariverost <catalinariverostroncoso@gmail.com>
2021-11-12 16:01:53 -0300

    actualizacion js
    primer intento de js

--- productos/js/productors.js
+++ productos/js/productors.js
@@ -1,6 +1,11 @@
+console.log("ola");
 const readMore = (id) => {
     var element = document.getElementById(id);
     element.classList.toggle("truncate");
     console.log(element);
   }
   
+  function haceAlgo() {
+    console.log("entro a la funcion")
+    
+  }
